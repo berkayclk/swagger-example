28608bde9649d5002076a38d7dac9928fe791f90
Berkay çelik <berkayc78@gmail.com>
2019-02-21 01:34:44 +0300

    adds appInfo to Swagger.

--- src/main/java/com/celik/example/swagger/config/SwaggerConfig.java
+++ src/main/java/com/celik/example/swagger/config/SwaggerConfig.java
@@ -1,10 +1,13 @@
 package com.celik.example.swagger.config;
 
+import springfox.documentation.service.Contact;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,8 +22,19 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()                 .apis(RequestHandlerSelectors.basePackage("com.celik.example.swagger.controllers"))
                 .paths(regex("/product.*"))
-                .build();
+                .build()
+                .apiInfo(metaData());
 
+    }
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot REST API")
+                .description("\"Spring Boot REST API for Swagger Example\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("Berkay Çelik", "https://berkaycelik.com.tr", "berkayc96@hotmail.com"))
+                .build();
     }
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
